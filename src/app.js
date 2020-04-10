2a14e83d00df29f6faa6d3208a8ae0cdbc149cfa
Gustavo Avila <gustavogavila01@gmail.com>
2020-04-10 17:17:56 -0300

    Resolução do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,41 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+function isItExist(request, response, next) {
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  if (repoIndex < 0)
+    return response.status(400).json({error: 'Repository not found.'});
+  request.id = {idString: repositories[repoIndex].id, repoIndex};
+  return next();
+}
+
+app.get("/repositories", (request, response) => response.json(repositories));
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepo = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(newRepo);
+  return response.status(201).json(newRepo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", isItExist, (request, response) => {
+  const { idString } = request.id;
+  const { title, url, techs, likes } = request.body;
+  if (likes && likes > 0) return response.json({ likes: 0 });
+  return response.status(200).json({ id: idString, title, url, techs });
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", isItExist, (request, response) => {
+  const { repoIndex } = request.id;
+  repositories.splice(repoIndex, 1);
+  return response.status(204).json();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", isItExist, (request, response) => {
+  const { repoIndex } = request.id;
+  repositories[repoIndex].likes += 1;
+  return response.json({likes: repositories[repoIndex].likes})
 });
 
 module.exports = app;
